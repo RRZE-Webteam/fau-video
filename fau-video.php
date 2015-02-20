8070ea2ab61e6033c56c9100ac63aad55ee35cea
Wolfgang Wiese <xwolf@xwolf.de>
2015-02-20 18:29:24 +0100

    bugfixing and open grapg

--- fau-video.php
+++ fau-video.php
@@ -38,7 +38,7 @@ class FAU_Video_Player {
     const textdomain = 'fau-video-player';
     const php_version = '5.3'; // Minimal erforderliche PHP-Version
     const wp_version = '4.0'; // Minimal erforderliche WordPress-Version
-    
+    static $embedscript;
     private $videoportal = array('www.video.uni-erlangen.de', 'www.video.fau.de', 'video.fau.de', 'www.fau-tv.de', 'fau-tv.de', 'www.fau.tv', 'fau.tv');
 
     protected static $instance = null;
@@ -53,14 +53,28 @@ class FAU_Video_Player {
         return self::$instance;
     }
 
-    private function init() {
+    public function init() {
         load_plugin_textdomain(self::textdomain, false, dirname(plugin_basename(__FILE__)) . '/languages/');
 
-        add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
         add_shortcode('fauvideo', array($this, 'shortcode'));
+	// if (self::$embedscript==true) {
+	 add_action('init', array($this, 'register_scripts'));
         //add_filter('oembed_fetch_url', array($this, 'oembed_url_filter'), 10, 3);
+//	}
+	//add_action('wp_footer', array($this, 'print_script'));
+	
     }
 
+    public function register_scripts() {      
+        wp_enqueue_script('fauvideo', plugins_url('/', __FILE__) . 'js/jwplayer.js', false, self::version);
+    }
+
+    public function print_script() {
+	//	if ( ! self::$embedscript )
+	//		return;
+		wp_print_scripts('fauvideo');
+	}
+    
     public static function activate() {
         self::version_compare();
         update_option(self::version_option_name, self::version);
@@ -88,54 +102,100 @@ class FAU_Video_Player {
             update_option(self::version_option_name, self::version);
     }
 
-    public function enqueue_scripts() {      
-        wp_enqueue_script('fauvideo', plugins_url('/', __FILE__) . 'js/jwplayer.js', false, self::version);
-    }
-
     public function shortcode($atts) {
         $default = array(
             'url' => '',
             'image' => '',
             'width' => '',
-            'height' => ''
+            'height' => '',
+	    'showtitle' => false,
+	    'showinfo'	=> false,
+	    'titletag' => ''
         );
         $atts = shortcode_atts($default, $atts);       
         extract($atts);
         if (empty($url)) {
             return __('Es wurde keine Adresse zu einem Video eingegeben.', self::textdomain);
         } else {
+	    $rand = rand();
             $host = parse_url($url, PHP_URL_HOST);    
             if (in_array($host, $this->videoportal)) {
                 $oembed_url = 'http://www.video.uni-erlangen.de/services/oembed/?url=' . $url . '&format=json';
-                $video = json_decode(wp_remote_retrieve_body(wp_safe_remote_get($oembed_url)), true);                         
+                $video = json_decode(wp_remote_retrieve_body(wp_safe_remote_get($oembed_url)), true);       
+		
+		$output = '';
+		$file = '';
                 if (isset($video['file'])) {
-                    $file = $video['file'];
-                }
-                if (empty($image)) {
-                    if (isset($video['image'])) {
-                        $image = $video['image'];
-                    } else {
-                        $image = plugins_url('/', __FILE__) . 'img/itunes_fau_800x400.png';
-                    } 
-                }
-                if (isset($video['width']) && isset($video['height'])) {
-                    if (empty($width)) {
-                        if (empty($height)) {
-                            $width = $video['width'];
-                            $height = $video['height'];
-                        } else {
-                            $width = ($video['width'] * $height) / $video['height'];
-                        }
-                    } else {
-                        if (empty($height)) {
-                            $height = ($video['height'] * $width) / $video['width'];                        
-                        }
-                    }
-                } 
-                $loading = __('Video wird geladen...', self::textdomain);
+                    $file = $video['file'];       
+		    
+		    self::$embedscript = true;
+		    
+		    if (filter_var($image, FILTER_VALIDATE_URL)) {
+			// nehme $image von der shortcodeeingabe
+		    } else {
+			if (empty($image)) {
+			    if (isset($video['image'])) {
+				$image = $video['image'];
+			    } else {
+				$image = plugins_url('/', __FILE__) . 'img/itunes_fau_800x400.png';
+			    } 
 
-                $output = "<div id='" . $url . "'>" . $loading . "</div>\n<script type='text/javascript'>\n   jwplayer('" . $url . "').setup({\n    flashplayer: '" . plugins_url('/', __FILE__) . 'js/player.swf' . "',\n    skin: '" . plugins_url('/', __FILE__) . 'skin/glow.zip' . "',\n    file: '" . $file . "',\n    image: '" . $image . "',\n    width: " . $width . ",\n    height: " . $height . "    });\n</script>";
-                return $output;
+			}
+		    }
+		    if (isset($video['width']) && isset($video['height'])) {
+			if (empty($width)) {
+			    if (empty($height)) {
+				$width = $video['width'];
+				$height = $video['height'];
+			    } else {
+				$width = ($video['width'] * $height) / $video['height'];
+			    }
+			} else {
+			    if (empty($height)) {
+				$height = ($video['height'] * $width) / $video['width'];                        
+			    }
+			}
+		    } 
+		    if (empty(trim($titletag))) {
+			$titletag = 'h2';
+		    }
+		    if ($showinfo==true) {
+			$showtitle = true;
+		    }
+		    $output .= '<div class="fauvideo-'.$rand.'" itemscope itemtype ="http://schema.org/Movie">';
+		    if (($showtitle ==true) && isset($video['title'])) {
+			$output .= "<$titletag itemprop=\"name\">".$video['title']."</$titletag>";
+		    }
+		    
+		    $output .= '<meta itemprop="contentUrl" content="'.$file.'">';
+		    $output .= '<meta itemprop="height" content="'.$video['height'].'">';
+		    $output .= '<meta itemprop="width" content="'.$video['width'].'">';
+		    if (isset($video['image'])) {
+			$output .= '<meta itemprop="thumbnailUrl" content="'.$video['image'].'">';
+		    }
+		    $loading = __('Video wird geladen...', self::textdomain);
+
+		    $output .= "<div id='video-" . $rand . "'>" . $loading . "</div>\n<script type='text/javascript'>\n   jwplayer('video-".$rand."').setup({\n    flashplayer: '" . plugins_url('/', __FILE__) . 'js/player.swf' . "',\n    skin: '" . plugins_url('/', __FILE__) . 'skin/glow.zip' . "',\n    file: '" . $file . "',\n    image: '" . $image . "',\n    width: " . $width . ",\n    height: " . $height . "    });\n</script>";
+		    
+		    if ($showinfo==true) {
+			 $output .= "<ul class=\"info\">\n";
+			 if (isset($video['author_name'])) {
+			    $output .= '<li>'.__('Vortrag',self::textdomain).': <span class="actor">'.$video['author_name'].'</span></li>'."\n";
+			 }
+			 $output .= '<li>'.__('Quelle',self::textdomain).': <a href="'.$url.'" class="isBasedOnUrl">'.$url.'</a></li>'."\n";
+			 if (isset($video['provider_name'])) {
+			    $output .= '<li>'.__('Copyright',self::textdomain).': <a href="'.$video['provider_url'].'"><span class="publisher">'.$video['provider_name'].'</span></a></li>'."\n";
+			 }
+			 $output .= "</ul>\n";
+		    }
+		    $output .= "</div>\n";
+		    
+		} else {
+		    return __('Die angegebene URL lieferte keine Videodaten. Bitte verwenden Sie die Adresse aus dem Videoportal, in dem ein Video als solches auch angezeigt wird und keine Indexseite.', self::textdomain);
+		}
+		
+		
+		return $output;
             } else {
                 return __('Es können nur Videos vom Videoportal eingebunden werden.', self::textdomain);
             }
